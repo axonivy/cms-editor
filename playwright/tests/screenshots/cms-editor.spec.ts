ebc2d79e35b3ede0b8a7d51c5b5d67a0d3f9ed29
Lukas Giger <lukas.giger@axonivy.com>
2025-11-11 15:32:45 +0100

    XIVY-17354 improve screenshot
    Select a Content Object before taking a screenshot of the general editor to make it look better.

--- playwright/tests/screenshots/cms-editor.spec.ts
+++ playwright/tests/screenshots/cms-editor.spec.ts
@@ -9,7 +9,9 @@ test.describe('CmsEditor', () => {
   });
 
   test('screenshots', async ({ page }) => {
+    await editor.main.table.row(0).locator.click();
     await editor.takeScreenshot('cms-editor.png');
+    await editor.main.table.header(0).locator.click();
 
     await editor.main.control.languageTools.trigger.click();
     await editor.main.control.languageTools.languageManager.trigger.click();
